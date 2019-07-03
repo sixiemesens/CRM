565eddb1e44cf289534419b4ab9226b203d1169b
atef.masghouni <atef.masghouni@esprit.tn>
2019-07-03 19:43:01 +0100

    relation 1-1 entre packs et offres avec géneration automatique

---
+++ CRM.Data/Configurations/PackConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CRM.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Data.Configurations
+{
+    class PackConfiguration: EntityTypeConfiguration<Packs>
+    {
+        public PackConfiguration()
+        {
+            HasOptional<Offers>(s => s.Offers).WithRequired(p => p.Packs);
+        }
+    }
+}
