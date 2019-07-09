39f48a91d1b8d8f93809c983fd3cca324954a921
atef.masghouni <atef.masghouni@esprit.tn>
2019-07-09 17:51:32 +0100

    Revert "changement DB"
    This reverts commit e4f775f46dd41c3e52f89b0026a00bcfa389f82f.

--- CRM/Models/IdentityModels.cs
+++ CRM/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace CRM.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("name=DB", throwIfV1Schema: false)
+            : base("CRM_FINAL", throwIfV1Schema: false)
         {
         }
 
